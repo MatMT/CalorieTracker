ff84cf3d3f973901a24a582f525ee54b21363b3a
EliDev <oscarmateoelias@gmail.com>
2024-08-09 14:41:02 -0600

    feat: Add ActivityList component with edit and delete buttons
    The code changes include:
    - Adding the ActivityList component in src/ActivityList.tsx.
    - Including edit and delete buttons for each activity in the component.
    - Styling the component with CSS classes for layout and appearance.
    - Reset App funcionality
    
    These changes enhance the functionality of the application by allowing users to edit and delete activities from the list.

--- src/ActivityList.tsx
+++ src/ActivityList.tsx
@@ -20,52 +20,64 @@ export default function ActivityList({
     [activities]
   );
 
+  const isEmptyActivities = useMemo(
+    () => activities.length === 0,
+    [activities]
+  );
+
   return (
     <>
       <h2 className="text-4xl font-bold text-slate-700 text-center">
         Comida y Actividades
       </h2>
 
-      {activities.map((activity) => (
-        <div
-          key={activity.id}
-          className="px-5 py-10 bg-white mt-5 flex justify-between"
-        >
-          <div className="space-y-2 relative">
-            <p
-              className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+      {isEmptyActivities ? (
+        <p className="text-center my-5">No hay registros aún...</p>
+      ) : (
+        activities.map((activity) => (
+          <div
+            key={activity.id}
+            className="px-5 py-10 bg-white mt-5 flex justify-between shadow-sm"
+          >
+            <div className="space-y-2 relative">
+              <p
+                className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
                 ${activity.category === 1 ? "bg-lime-500" : "bg-orange-500"} `}
-            >
-              {categoryName(+activity.category)}
-            </p>
-            <p className="text-2xl font-bold pt-5 ">{activity.name}</p>
-            <p className="font-black text-4xl text-lime-500">
-              {activity.calories}
-            </p>
-          </div>
+              >
+                {categoryName(+activity.category)}
+              </p>
+              <p className="text-2xl font-bold pt-5 ">{activity.name}</p>
+              <p className="font-black text-4xl text-lime-500">
+                {activity.calories}
+              </p>
+            </div>
 
-          <div className="flex gap-5 items-center">
-            <button
-              onClick={() =>
-                dispatch({ type: "set-activeId", payload: { id: activity.id } })
-              }
-            >
-              <PencilSquareIcon className="h-8 w-8 text-gray-800"></PencilSquareIcon>
-            </button>
+            <div className="flex gap-5 items-center">
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: "set-activeId",
+                    payload: { id: activity.id },
+                  })
+                }
+              >
+                <PencilSquareIcon className="h-8 w-8 text-gray-800"></PencilSquareIcon>
+              </button>
 
-            <button
-              onClick={() =>
-                dispatch({
-                  type: "delete-activity",
-                  payload: { id: activity.id },
-                })
-              }
-            >
-              <XCircleIcon className="h-8 w-8 text-red-600"></XCircleIcon>
-            </button>
+              <button
+                onClick={() =>
+                  dispatch({
+                    type: "delete-activity",
+                    payload: { id: activity.id },
+                  })
+                }
+              >
+                <XCircleIcon className="h-8 w-8 text-red-600"></XCircleIcon>
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import { useReducer } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./ActivityList";
 
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
+
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length > 0, [state.activities]);
 
   return (
     <>
@@ -14,7 +21,13 @@ function App() {
             Contador de Calorías
           </h1>
 
-          <button>Reiniciar</button>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 font-bold uppercase cursor-pointer text-sm text-white rounded-lg p-2 disabled:opacity-20"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
 
